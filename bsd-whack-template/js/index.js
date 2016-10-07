788c5f2a1e4a82060aa8729967bf8570ce2e9240
JesperJ123 <17jarlefeltjc1@webmail.sis.edu.hk>
2016-10-07 13:46:55 +0800

    importing my whack rat game

---
+++ bsd-whack-template/js/index.js
@@ -0,0 +1,49 @@
+//ADD JQUERY AND JQUERY UI BEFORE YOU WRITE YOUR JS CODE
+
+$(document).ready(function() {
+
+  //ALL your JS code goes here!
+
+  //What happens when you click an enemy
+  $(document).ready(function() {
+    var timeleft = 60;
+  }
+
+  $(".rat").click(function() {
+    $(this).hide();
+    setTimeout(function() {
+      $('.rat').show();
+    }, 1000);
+  });
+
+}); //closing bracket for document.ready
+
+$(document).ready(function() {
+  //all code goes here
+  var score = 0;
+
+  $('.rat').click(function() {
+    //this code runs when f1 clicked
+    score = score + 1;
+    document.getElementById('points').innerHTML = score;
+  });
+
+  $('.#').click(function() {
+    //this code runs when f1 clicked
+    score = score - 1;
+
+    document.getElementById('points').innerHTML = score;
+  });
+
+  //this is for the reset button
+
+  document.getElementById('points').innerHTML = score;
+  $('.rat').hide();
+  //the this keyword refers to the element that was clicked to the function
+
+  //setTimeout function means that the function will not run after a delay -to this case it's after 2000ms/2 (miliesconds)//
+  setTimeout(function() {
+    $(',sun').fadeIn();
+  }, 2000);
+
+});
